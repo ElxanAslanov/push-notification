1deb160891690ed87b2bbbf15684ec184235ae71
Elxan123 <elxan.aslanov2000@gmail.com>
2021-03-09 22:40:33 +0400

    checks1

--- psuh.js
+++ psuh.js
@@ -7,7 +7,7 @@ const vapIdKey = {
 
 push.setVapidDetails('mailto:fkake123@gmail.com',vapIdKey.publicKey,vapIdKey.privateKey)
 
-let sub = {}
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/d8USlcznS04:APA91bH0QoDPP_1U-AFOgOLAxXskMS8lv3imdfqMdKrkZyZslEdDpL7vuAN4Jv9LRaZ4gmxkLdj442qVHCCcO8JCZajI7HgNNo8CdxU-vG5pwCgr2BE0B5OQqXGmVPNADzl3furt0dU9","expirationTime":null,"keys":{"p256dh":"BOxefrsyJzq5fk4FscHx6NfuVxKGoLkg9k45Mkwq0PYOCS_LnQXRhcEeHU0HiQLX6thSOr6CwU3-GSwnh1zWCks","auth":"snRGJvxKVajjl8URZm-ANg"}}
 
-push.sendNotification(sub,'my message')
+push.sendNotification(sub,'my message --------aaaaaaaaaaaaaaaa')
 
